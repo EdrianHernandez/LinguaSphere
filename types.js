fb6b0511977aec9a8e87094bc5ad3663997b8f86
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-11 23:52:24 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,11 @@
+/**
+ * Supported language options for the application.
+ */
+export const SupportedLanguage = {
+  SPANISH: 'Spanish',
+  FRENCH: 'French',
+  GERMAN: 'German',
+  JAPANESE: 'Japanese',
+  ITALIAN: 'Italian',
+  CHINESE: 'Chinese'
+};
